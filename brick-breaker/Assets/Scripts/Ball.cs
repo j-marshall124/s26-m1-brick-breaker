4d8e9a0f73c6a0e8a753c2741d8251e0d6f826d3
j-marshall124 <jordan.marshall1@mohawkcollege.ca>
2026-05-29 12:54:05 -0400

    Add ball movement

--- brick-breaker/Assets/Scripts/Ball.cs
+++ brick-breaker/Assets/Scripts/Ball.cs
@@ -2,15 +2,86 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Controls the ball in play
+    public Rigidbody2D rb2d;
+    // Speed of ball at launch
+    public float launchSpeed = 100; // in px
+    //
+    public BlockManager blockManager;
+
+    // Used to reset ball position
+    private Vector3 startPosition;
+
     void Start()
     {
-        
+        // Record where the ball began in the scene
+        startPosition = transform.position;
+
+        // Begin play
+        LaunchBall();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        
+        if (rb2d == null)
+            rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Block") == true)
+        {
+            // remove block from scene
+            Destroy(collision.gameObject);
+            // have block manager count that
+            blockManager.numberOfBlocksDestroyed += 1;
+        }
+
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            CollideWithRacket(collision.collider);
+        }
+    }
+
+    void LaunchBall()
+    {
+        // Move ball back to starting position when we ran the game / scene
+        rb2d.MovePosition(startPosition);
+        // Add velocity to ball: give it a direction, give it a speed
+        rb2d.linearVelocity = Vector2.up * launchSpeed;
+    }
+
+    void CollideWithRacket(Collider2D racket)
+    {
+        // Subtract racket position from ball position
+        // This gives X relative to centre of paddle
+        float relativeXofBall = this.transform.position.x - racket.transform.position.x;
+
+        float boundWidth = racket.bounds.size.x;
+        float boundWidthThird = boundWidth / 3;
+
+        bool isCentred = Mathf.Abs(relativeXofBall) < boundWidthThird / 2;
+        if (isCentred)
+        {
+            // nothing?
+        }
+        else // in third on either side
+        {
+            // if negative == left side
+            if (relativeXofBall < 0)
+            {
+                Vector2 direction = rb2d.linearVelocity.normalized + Vector2.left;
+                direction.Normalize();
+                rb2d.linearVelocity = direction * rb2d.linearVelocity.magnitude;
+            }
+
+            // if negative == right side
+            if (relativeXofBall > 0)
+            {
+                Vector2 direction = rb2d.linearVelocity.normalized + Vector2.right;
+                direction.Normalize();
+                rb2d.linearVelocity = direction * rb2d.linearVelocity.magnitude;
+            }
+        }
     }
 }
--- brick-breaker/Assets/Scripts/BlockManager.cs
+++ brick-breaker/Assets/Scripts/BlockManager.cs
@@ -9,7 +9,12 @@ public class BlockManager : MonoBehaviour
     // Gap between blocks (in pixels)
     public int blockGap = 2;
     // Define a grid using numbers as index
+    [TextArea(10, 10)] // Creates a textbox area in Unity inspector
     public string gridBlocks = "000 222 000\n22222222222\n22 333 22\n333";
+
+    // How many blocks were created for this grid?
+    public int numberOfBlocks;
+    public int numberOfBlocksDestroyed;
 
     void Start()
     {
@@ -27,7 +32,7 @@ public class BlockManager : MonoBehaviour
         for (int y = 0; y < rows.Length; y++)
         {
             // Grab single row from array of rows
-            string row = rows[y];
+            string row = rows[y].Trim(); // .Trim removes white space (such as "\r")
             // Calculate the total width of the row
             float rowWidth = blockSize.x * row.Length + blockGap * (row.Length - 1);
             // Calculate spacing for block along Y ais (space each row)
@@ -41,6 +46,7 @@ public class BlockManager : MonoBehaviour
                 if (blockStr == " ")
                     continue;
 
+
                 // (rowWidth / 2) -- means offset to left side by half the width of row; centre adjusted
                 // (blockSize.x + blockGap) * x -- means offset new block by how many came before it
                 // (blockSize.x / 2) -- offset by half block width moved to the right
@@ -49,6 +55,7 @@ public class BlockManager : MonoBehaviour
                 // Create new block
                 Vector3 position = new Vector3(blockX, blockY, 0) + this.transform.position;
                 GameObject block = Instantiate(blockPrefab, position, Quaternion.identity, this.transform);
+                numberOfBlocks += 1;
                 // Convert string values 0, 1, 2, 3, or 4 into integer to get sprite from array of sprites and assign it
                 int spriteIndex = int.Parse(blockStr);
                 Sprite blockSprite = blockSprites[spriteIndex];
